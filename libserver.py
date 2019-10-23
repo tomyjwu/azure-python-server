e65243687e3d0c4443072df2ba19b77b59afbdf6
Tom Wu <tom.wu@genie-networks.com>
2019-10-23 14:42:33 +0800

    remove unicode

--- libserver.py
+++ libserver.py
@@ -5,8 +5,8 @@ import io
 import struct
 
 request_search = {
-    "morpheus": "Follow the white rabbit. \U0001f430",
-    "ring": "In the caves beneath the Misty Mountains. \U0001f48d",
+    "morpheus": "Follow the white rabbit.",
+    "ring": "In the caves beneath the Misty Mountains.",
     "\U0001f436": "\U0001f43e Playing ball! \U0001f3d0",
 }
 
